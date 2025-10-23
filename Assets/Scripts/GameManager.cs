8a8db7bf00c6c63729abd0fe7d05dc606d87c5e4
Natan Camargos <natanscamargos@gmail.com>
2025-10-23 16:16:26 -0300

    hazard at same z as the player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,38 +19,39 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-	if (gameOver)
-	{
-	    return;
-	}
-
-	time += Time.deltaTime;
-
-	if (time >= 1f)
-	{
-	    score++;
-	    scoreText.text = score.ToString();
-	    time = 0;
-	}
+        if (gameOver)
+        {
+            return;
+        }
+       
+        time += Time.deltaTime;
+       
+        if (time >= 1f)
+        {
+            score++;
+            scoreText.text = score.ToString();
+            time = 0;
+        }
     }
     
     private IEnumerator SpawnHazards()
     {
-	var hazardsToSpawn = Random.Range(1, maxHazardsToSpawn);
-	for (int i = 0; i <= hazardsToSpawn; i++)
-	{
-	    var x = Random.Range(-6, 6);
+        var hazardsToSpawn = Random.Range(1, maxHazardsToSpawn);
+        for (int i = 0; i <= hazardsToSpawn; i++)
+        {
+            var x = Random.Range(-6, 6);
+	    var z = player.GetComponent<Transform>().position.z;
             var drag = Random.Range(0f, 2f);
-	    var hazard = Instantiate(hazardPrefab, new Vector3(x, 12, player.GetComponent<Transform>().position.z), Quaternion.identity);
-	    hazard.GetComponent<Rigidbody>().drag = drag;
-	}
-	yield return new WaitForSeconds(1f);
-
-	yield return SpawnHazards();
+            var hazard = Instantiate(hazardPrefab, new Vector3(x, 12, z), Quaternion.identity);
+            hazard.GetComponent<Rigidbody>().drag = drag;
+        }
+        yield return new WaitForSeconds(1f);
+       
+        yield return SpawnHazards();
     }
 
     public static void GameOver()
     {
-	gameOver = true;
+        gameOver = true;
     }
 }
